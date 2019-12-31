a5f797c867ec97a5b84a6425ad6520e2fa136eef
qiaogaojian <qiaogaojian@vip.qq.com>
2019-12-31 14:27:43 +0800

    解决白代码问题

--- app/src/main/java/com/etatech/test/utils/BaseActivity.java
+++ app/src/main/java/com/etatech/test/utils/BaseActivity.java
@@ -3,9 +3,13 @@ package com.etatech.test.utils;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.blankj.utilcode.util.AdaptScreenUtils;
 import com.blankj.utilcode.util.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Michael
@@ -15,11 +19,11 @@ import com.blankj.utilcode.util.ScreenUtils;
 public class BaseActivity extends AppCompatActivity
 {
     @Override
-public Resources getResources() {
-    if (ScreenUtils.isPortrait()) {
-        return AdaptScreenUtils.adaptWidth(super.getResources(), 1080);
-    } else {
-        return AdaptScreenUtils.adaptHeight(super.getResources(), 1080);
+    public Resources getResources() {
+        if (ScreenUtils.isPortrait()) {
+            return AdaptScreenUtils.adaptWidth(super.getResources(), 1080);
+        } else {
+            return AdaptScreenUtils.adaptHeight(super.getResources(), 1080);
+        }
     }
-}
 }
