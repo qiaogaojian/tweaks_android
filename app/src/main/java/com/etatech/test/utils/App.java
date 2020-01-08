5ba5e5d8a68434d87b3d0bdbb812270d924ccdc8
qiaogaojian <qiaogaojian@vip.qq.com>
2020-01-08 14:55:05 +0800

    注册app

--- app/src/main/java/com/etatech/test/utils/App.java
+++ app/src/main/java/com/etatech/test/utils/App.java
@@ -29,12 +29,8 @@ public class App extends Application {
     }
 
     // 如果是悬浮窗适配，因为 inflate 用到的 context 是 application 级别的，所以需要在自定义的 Application 中重写 getResource。
-    @Override
-    public Resources getResources() {
-        if (ScreenUtils.isPortrait()) {
-            return AdaptScreenUtils.adaptWidth(super.getResources(), 1080);
-        } else {
-            return AdaptScreenUtils.adaptHeight(super.getResources(), 1080);
-        }
-    }
+//    @Override
+//    public Resources getResources() {
+//        return AdaptScreenUtils.adaptWidth(super.getResources(), 1080);
+//    }
 }
