2b5d927f3c264ee379c05300e9b3c1954e56d327
qiaogaojian <qiaogaojian@vip.qq.com>
2020-01-19 10:11:15 +0800

    测试静态View内存泄漏

--- app/src/main/java/com/etatech/test/view/LeakStaticViewActivity.java
+++ app/src/main/java/com/etatech/test/view/LeakStaticViewActivity.java
@@ -1,22 +1,48 @@
 package com.etatech.test.view;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.blankj.utilcode.util.ImageUtils;
+import com.bumptech.glide.Glide;
 import com.etatech.test.R;
 import com.etatech.test.databinding.ActivityLeakStaticViewBinding;
 import com.etatech.test.utils.BaseActivity;
 
 public class LeakStaticViewActivity extends BaseActivity<ActivityLeakStaticViewBinding> {
 
+    private static ImageView ivLena;
+
     @Override
     public ActivityLeakStaticViewBinding onCreateView(Bundle savedInstanceState) {
-        return DataBindingUtil.setContentView(this,R.layout.activity_leak_static_view);
+        return DataBindingUtil.setContentView(this, R.layout.activity_leak_static_view);
     }
 
     @Override
     public void init() {
+        Glide.with(this)
+                .load(R.drawable.image_lena)
+                .into(binding.ivStaticView);
 
+        ivLena = binding.ivStaticView;
+        PropertyValuesHolder scaleAniX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1F, 2F, 1F);
+        PropertyValuesHolder scaleAniY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1F, 2F, 1F);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(binding.ivStaticView, scaleAniX, scaleAniY);
+        animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ivLena = null;
     }
 }
