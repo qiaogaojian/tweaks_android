c6461a29925c60f0ec1dc4ea59b6a90d634c844e
qiaogaojian <qiaogaojian@vip.qq.com>
2020-02-21 15:43:29 +0800

    添加splash界面

--- app/src/main/java/com/etatech/test/view/SplashActivity.java
+++ app/src/main/java/com/etatech/test/view/SplashActivity.java
@@ -22,7 +22,59 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
 
     @Override
     public void init() {
-        if (getIntent() == null) {
+        if (getIntent() == null || getIntent().getExtras() == null) {
+            SplashAdBean adBean = new SplashAdBean();
+            adBean.setAdUrl("https://www.douyu.com/510229");
+            adBean.setResUrl("https://upload-images.jianshu.io/upload_images/2229730-5cd1dab1a302b122.gif");
+            adBean.setStayTime(5);
+            adBean.setType("gif");
+            SplashAd splashAd = new SplashAd(this, adBean, binding.splashContainer, new SplashAdListener() {
+                @Override
+                public void onSplashAdSuccessToShow() {
+                    LogUtils.e("展示成功");
+                }
+
+                @Override
+                public void onSplashAdFailToShow() {
+                    LogUtils.e("展示失败");
+                }
+
+                @Override
+                public void onSplashAdClicked() {
+                    LogUtils.e("点击广告");
+                }
+
+                @Override
+                public void onSplashAdClose() {
+                    LogUtils.e("跳过广告");
+                    Intent intent = new Intent();
+                    intent.setClass(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onSplashAdFinish() {
+                    LogUtils.e("广告播放完成");
+                    Intent intent = new Intent();
+                    intent.setClass(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            // 网络差或失败时的默认广告图
+            splashAd.setDefaultCover(getResources().getDrawable(R.drawable.splash_bg));
+            // 字体
+            splashAd.setTypeface(App.getInstance().getTypeface());
+            // 应用icon
+            splashAd.setLogo(getResources().getDrawable(R.drawable.logo));
+            // 下方 copyright
+            splashAd.setCopyRight("Copyright 2016 - 2019 Mega Information Technology Co.,Ltd. All Rights Reserved");
+            // 倒计时后方文字
+            splashAd.setJumpText("跳过");
+            // 资源下载等待时间
+            splashAd.setWaitTime(2);
+
+            splashAd.show();
             return;
         }
         if (getIntent().getExtras().getBoolean("noAd", false)) {
@@ -46,7 +98,7 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
                 public void onSplashAdClose() {
                     LogUtils.e("跳过广告");
                     Intent intent = new Intent();
-                    intent.setClass(SplashActivity.this,MainActivity.class);
+                    intent.setClass(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
 
@@ -54,7 +106,7 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
                 public void onSplashAdFinish() {
                     LogUtils.e("广告播放完成");
                     Intent intent = new Intent();
-                    intent.setClass(SplashActivity.this,MainActivity.class);
+                    intent.setClass(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
             });
@@ -85,7 +137,7 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
                 public void onSplashAdClose() {
                     LogUtils.e("跳过广告");
                     Intent intent = new Intent();
-                    intent.setClass(SplashActivity.this,MainActivity.class);
+                    intent.setClass(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
 
@@ -93,7 +145,7 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
                 public void onSplashAdFinish() {
                     LogUtils.e("广告播放完成");
                     Intent intent = new Intent();
-                    intent.setClass(SplashActivity.this,MainActivity.class);
+                    intent.setClass(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
             });
