80c6d8d45ced67d726cd12bc8398cfc5914cdf6b
qiaogaojian <qiaogaojian@vip.qq.com>
2020-03-25 15:35:10 +0800

    Java code test

---
+++ app/src/main/java/com/etatech/test/Java/JavaTest.java
@@ -0,0 +1,14 @@
+package com.etatech.test.Java;
+
+/**
+ * Created by Michael
+ * Date:  2020/3/25
+ * Func:
+ */
+public class JavaTest {
+
+    public static void main(String args[]) {
+        int num = 666;
+        System.out.println(String.format("Hello World %d", num));
+    }
+}
---
+++ app/src/main/java/com/etatech/test/Java/Rxjava/RxJavaTest.java
@@ -0,0 +1,13 @@
+package com.etatech.test.Java.Rxjava;
+
+/**
+ * Created by Michael
+ * Date:  2020/3/25
+ * Func:
+ */
+public class RxJavaTest {
+
+    public static void main(String args[]) {
+
+    }
+}
