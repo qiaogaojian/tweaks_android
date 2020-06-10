fa03a4c0f437f30a213cb6ec36c82bf5c525a63f
qiaogaojian <qiaogaojian@vip.qq.com>
2020-06-10 17:15:14 +0800

    实现shader自定义view 闪光的textview

--- app/src/main/java/com/etatech/test/view/custom/shader/TestLinearGradientShaderView.java
+++ app/src/main/java/com/etatech/test/view/custom/shader/TestLinearGradientShaderView.java
@@ -2,6 +2,7 @@ package com.etatech.test.view.custom.shader;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -10,7 +11,10 @@ import android.graphics.Rect;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
+
+import com.etatech.test.R;
 
 /**
  * Created by Michael
@@ -22,6 +26,17 @@ public class TestLinearGradientShaderView extends View {
     private Paint textPaint;
     private Paint rectPaint;
     private LinearGradient linearGradient;
+    private int viewHeight;
+    private int viewWidth;
+    private int textHeight;
+    private int textWidth;
+    private int textTopPos;
+    private int textDownPos;
+
+    private String text;
+    private int textSize;
+    private int gradientWidth;
+
     // 渐变位置
     private float[] positions = {0.3f, 0.6f, 0.8f};
     // 渐变颜色
@@ -30,22 +45,43 @@ public class TestLinearGradientShaderView extends View {
     public TestLinearGradientShaderView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        final int height = getMeasuredHeight();
-        final int width = getMeasuredWidth();
+        TypedArray arr = getContext().obtainStyledAttributes(attrs, R.styleable.TestLinearGradientShaderView);
+        text = arr.getString(R.styleable.TestLinearGradientShaderView_gradient_text);
+        textSize = arr.getInteger(R.styleable.TestLinearGradientShaderView_gradient_text_size, 30);
+        gradientWidth = arr.getInteger(R.styleable.TestLinearGradientShaderView_gradient_width, 30);
 
         rectPaint = new Paint();
         textPaint = new Paint();
         textPaint.setTextSize(100);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewHeight = h;
+        viewWidth = w;
+
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        textHeight = (int) Math.ceil(fontMetrics.descent - fontMetrics.ascent);
+        textWidth = (int) textPaint.measureText("TEST SHADER");
+        textTopPos = (int) Math.ceil((viewHeight - textHeight) / 2.0);
+        textDownPos = (int) Math.ceil((viewHeight + textHeight) / 2.0);
+
+        int leftBound = -gradientWidth;
+        int rightBound = viewWidth;
 
         ValueAnimator valueAnimator = new ValueAnimator();
-        valueAnimator.setIntValues(-1000, 1000);
+        valueAnimator.setIntValues(leftBound, rightBound);
         valueAnimator.setDuration(10000);
         valueAnimator.setRepeatCount(-1);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 offset = (int) animation.getAnimatedValue();
-                linearGradient = new LinearGradient(offset, 300, 1000 + offset, 600, colors, positions, Shader.TileMode.CLAMP);
+                linearGradient = new LinearGradient(
+                        offset, textTopPos,
+                        gradientWidth + offset, textDownPos,
+                        colors, positions, Shader.TileMode.CLAMP);
                 textPaint.setShader(linearGradient);
                 rectPaint.setShader(linearGradient);
                 invalidate();
@@ -60,10 +96,11 @@ public class TestLinearGradientShaderView extends View {
         super.onDraw(canvas);
         canvas.drawColor(Color.parseColor("#232323"));
         textPaint.setAlpha(255);
-        canvas.drawText("TEST SHADER", 200, 500, textPaint);
+        canvas.drawText("TEST SHADER", textTopPos, textDownPos, textPaint);
 
         rectPaint.setStyle(Paint.Style.STROKE);
         rectPaint.setStrokeWidth(3);
-        canvas.drawRect(new Rect(offset, 300, 1000 + offset, 600), rectPaint);
+        canvas.drawRect(new Rect(offset, textTopPos,
+                gradientWidth + offset, textDownPos), rectPaint);
     }
 }
