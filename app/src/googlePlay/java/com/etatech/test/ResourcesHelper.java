ea3679707b1b0eaf6165e822a4319afbfcc301de
qiaogaojian <qiaogaojian@vip.qq.com>
2020-07-10 11:50:21 +0800

    goole play分包: 自动打包obb

---
+++ app/src/googlePlay/java/com/etatech/test/ResourcesHelper.java
@@ -0,0 +1,16 @@
+package com.etatech.test;
+
+/**
+ * Created by Michael
+ * Date:  2020/7/6
+ * Func:
+ */
+public class ResourcesHelper {
+
+    //    public static void playSplashVideoResource(VideoView videoView) {
+    //        String filePath = ObbHelper.getCurrentObbFileFolder() + "raw/" + "splash.mp4";
+    //        videoView.setVideoPath(filePath);
+    //    }
+
+
+}
---
+++ app/src/normal/java/com/etatech/test/ResourcesHelper.java
@@ -0,0 +1,17 @@
+package com.etatech.test;
+
+/**
+ * Created by Michael
+ * Date:  2020/7/6
+ * Func:
+ */
+public class ResourcesHelper {
+
+    //    public static void playSplashVideoResource(VideoView videoView) {
+    //        int resource = R.raw.splash;
+    //        String uri = "android.resource://" + videoView.getContext().getApplicationContext().getPackageName() + "/" + resource;
+    //        videoView.setVideoURI(Uri.parse(uri));
+    //    }
+
+
+}
