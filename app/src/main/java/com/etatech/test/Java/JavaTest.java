0e4113312729020fa776d24723f0a1330ebb019e
qiaogaojian <qiaogaojian@vip.qq.com>
2020-07-22 14:33:30 +0800

    测试foreach遍历并修改集合

--- app/src/main/java/com/etatech/test/Java/JavaTest.java
+++ app/src/main/java/com/etatech/test/Java/JavaTest.java
@@ -1,5 +1,7 @@
 package com.etatech.test.Java;
 
+import java.util.LinkedList;
+
 /**
  * Created by Michael
  * Date:  2020/3/25
@@ -8,7 +10,46 @@ package com.etatech.test.Java;
 public class JavaTest {
 
     public static void main(String args[]) {
+        System.out.println("android"+testNull());
+    }
+
+    public static void hello() {
         int num = 666;
         System.out.println(String.format("Hello World %d", num));
+    }
+
+    public static String testNull() {
+        return null;
+    }
+
+    public static void testLinklist() {
+        LinkedList<TestBean> linkedList = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            linkedList.add(new TestBean(i+""));
+        }
+        for (TestBean s : linkedList) {
+            if (s.getValue().equals("6")) {
+                s.setValue("666");
+            }
+        }
+        for (TestBean s : linkedList) {
+            System.out.println(s.getValue());
+        }
+    }
+
+    static class TestBean {
+        String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public TestBean(String value) {
+            this.value = value;
+        }
     }
 }
