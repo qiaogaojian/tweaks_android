c34d0185154ba418907b543237b52c5f7f0ce972
qiaogaojian <qiaogaojian@vip.qq.com>
2020-10-24 09:28:11 +0800

    自定义vie 自定义宽高

--- app/src/main/java/com/etatech/test/view/practice8/PM25View_Practice_Exactly_200dp.java
+++ app/src/main/java/com/etatech/test/view/practice8/PM25View_Practice_Exactly_200dp.java
@@ -8,10 +8,10 @@ import static android.view.View.MeasureSpec.EXACTLY;
 
 /**
  * MeasureSpec.EXACTLY 场景
- *
+ * <p>
  * layout_width = "200dp"
  * 固定设定为 200dp 的宽度 或 高度，那么 getMeasuredWidth() 就返回了应有的固定宽度
- *
+ * <p>
  * 可以关注下面注释中带有 ---- 的部分
  */
 
@@ -45,8 +45,8 @@ public class PM25View_Practice_Exactly_200dp extends PM25View {
             case EXACTLY:
                 // ----
                 // 指定 200dp，MeasureSpec.getMode 会返回 EXACTLY
-//                Log.w(TAG, "width mode == exactly");
-//                w = getMeasuredWidth();
+                Log.w(TAG, "width mode == exactly");
+                w = getMeasuredWidth();
                 break;
         }
 
@@ -59,8 +59,8 @@ public class PM25View_Practice_Exactly_200dp extends PM25View {
             case EXACTLY:
                 // ----
                 // 指定 200dp，MeasureSpec.getMode 会返回 EXACTLY
-//                Log.w(TAG, "height mode == exactly");
-//                h = getMeasuredHeight();
+                Log.w(TAG, "height mode == exactly");
+                h = getMeasuredHeight();
                 break;
         }
 
@@ -73,8 +73,8 @@ public class PM25View_Practice_Exactly_200dp extends PM25View {
         // ----
         // 如果没有 resolveSize，这12次onMeasure过程当中有时候得到的宽度或高度为0
         // 加上 resolveSize 则不会出现这个情况，从第一次到最后一次都能得到固定值
-//        w = resolveSize(w, widthMeasureSpec);
-//        h = resolveSize(h, widthMeasureSpec);
+        w = resolveSize(w, widthMeasureSpec);
+        h = resolveSize(h, widthMeasureSpec);
 
         // 强制宽高相等
         if (h > w) {
