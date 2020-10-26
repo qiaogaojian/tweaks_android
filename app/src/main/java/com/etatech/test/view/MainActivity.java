4b0c8904d3b03938ff7654f400fcdd0e9ae53fa3
qiaogaojian <qiaogaojian@vip.qq.com>
2020-10-26 14:54:49 +0800

    自定义view 练习demo

--- app/src/main/java/com/etatech/test/view/MainActivity.java
+++ app/src/main/java/com/etatech/test/view/MainActivity.java
@@ -251,6 +251,13 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> implements V
                 startActivity(intent);
             }
         });
+        ClickUtil.setOnClick(binding.btnTestCustomviewDemo, new Action1() {
+            @Override
+            public void call(Object o) {
+                intent.setClass(MainActivity.this,TestCustomViewDemoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
