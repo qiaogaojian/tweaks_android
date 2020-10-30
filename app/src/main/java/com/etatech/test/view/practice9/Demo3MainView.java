4e99a7c1f30a82f5c67d87607774d188f9c7b42d
qiaogaojian <qiaogaojian@vip.qq.com>
2020-10-30 14:52:51 +0800

    添加自定义view3 小米运动

---
+++ app/src/main/java/com/etatech/test/view/practice9/Demo3MainView.java
@@ -0,0 +1,29 @@
+package com.etatech.test.view.practice9;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael
+ * Date:  2020/10/30
+ * Desc:
+ */
+public class Demo3MainView extends ViewGroup {
+    public Demo3MainView(Context context) {
+        super(context);
+    }
+
+    public Demo3MainView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Demo3MainView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
---
+++ app/src/main/java/com/etatech/test/view/practice9/Demo3View.java
@@ -0,0 +1,25 @@
+package com.etatech.test.view.practice9;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Michael
+ * Date:  2020/10/30
+ * Desc:
+ */
+public class Demo3View extends View {
+    public Demo3View(Context context) {
+        super(context);
+    }
+
+    public Demo3View(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Demo3View(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
