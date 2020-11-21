2f7653987b8c2c7bf18bfb5126d61a316c712cbe
qiaogaojian <qiaogaojian@vip.qq.com>
2020-11-21 10:12:50 +0800

    兼容多点触控的拖拽操作

---
+++ app/src/main/java/com/etatech/test/view/practice9/Demo15View.java
@@ -0,0 +1,139 @@
+package com.etatech.test.view.practice9;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.graphics.Region;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.etatech.test.utils.Tools;
+
+/**
+ * Created by Michael
+ * Date:  2020/11/20
+ * Desc:
+ */
+public class Demo15View extends View {
+
+    private Paint paint;
+    private PointF centerPos;
+    private ObjectAnimator ani;
+
+    private int radius = Tools.pt2Px(100);
+    private PointF circlePos;
+    private Region circleRegion;
+    private Path circlePath;
+
+
+    public Demo15View(Context context) {
+        super(context);
+        init();
+    }
+
+    public Demo15View(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.parseColor("#F7F7F0"));
+        centerPos = new PointF();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        centerPos.set(widthMeasureSpec / 2, heightMeasureSpec / 2);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerPos.set(w / 2, h / 2);
+        circlePos = centerPos;
+        circlePath = new Path();
+        circlePath.addCircle(circlePos.x, circlePos.y, radius, Path.Direction.CW);
+        circleRegion = new Region();
+        circleRegion.setPath(circlePath, new Region((int) circlePos.x - radius, (int) circlePos.y - radius, (int) circlePos.x + radius, (int) circlePos.y + radius));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(circlePos.x, circlePos.y, radius, paint);
+    }
+
+    PointF lastPos = new PointF();
+    PointF lastCirclePos = new PointF();
+    PointF curPos = new PointF();
+    private boolean canDrag = false;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+                int i = event.getPointerId(event.getActionIndex());
+                if (i == 0 && circleRegion.contains((int) event.getX(), (int) event.getY())) {
+                    canDrag = true;
+                }
+                lastPos.set(event.getX(), event.getY());
+                lastCirclePos.set(circlePos.x, circlePos.y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (canDrag) {
+                    int index = event.findPointerIndex(0);
+                    curPos.set(event.getX(index), event.getY(index));
+                    PointF offset = new PointF(curPos.x - lastPos.x, curPos.y - lastPos.y);
+                    circlePos.set(lastCirclePos.x + offset.x, lastCirclePos.y + offset.y);
+                    circlePath.reset();
+                    circlePath.addCircle(circlePos.x, circlePos.y, radius, Path.Direction.CW);
+                    circleRegion = new Region();
+                    circleRegion.setPath(circlePath, new Region((int) circlePos.x - radius, (int) circlePos.y - radius, (int) circlePos.x + radius, (int) circlePos.y + radius));
+                    invalidate();
+                }
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                if (event.getPointerId(event.getActionIndex()) == 0) {
+                    canDrag = false;
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        startAni();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        endAni();
+    }
+
+    private void startAni() {
+
+    }
+
+    private void endAni() {
+        if (ani != null) {
+            ani.end();
+        }
+    }
+
+}
