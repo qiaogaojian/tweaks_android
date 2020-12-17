86448f51c54c6341c7575686bbdd4ac5a2c38882
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-17 17:01:18 +0800

    测试带表情的字符串截取

--- app/src/main/java/com/etatech/test/view/MainActivity.java
+++ app/src/main/java/com/etatech/test/view/MainActivity.java
@@ -14,14 +14,19 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeUnit;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+
+import static java.lang.Character.isHighSurrogate;
+import static java.lang.Character.isLowSurrogate;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> implements View.OnClickListener {
     private Button btnAdaptWidth;
@@ -254,17 +259,97 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> implements V
         ClickUtil.setOnClick(binding.btnTestCustomviewDemo, new Action1() {
             @Override
             public void call(Object o) {
-                intent.setClass(MainActivity.this,TestCustomViewDemoActivity.class);
+                intent.setClass(MainActivity.this, TestCustomViewDemoActivity.class);
                 startActivity(intent);
             }
         });
         ClickUtil.setOnClick(binding.btnTestBezier, new Action1() {
             @Override
             public void call(Object o) {
-                intent.setClass(MainActivity.this,TestBezierActivity.class);
+                intent.setClass(MainActivity.this, TestBezierActivity.class);
                 startActivity(intent);
             }
         });
+        ClickUtil.setOnClick(binding.btnTestPrintStack, new Action1() {
+            @Override
+            public void call(Object o) {
+                // 第一种打印调用堆栈方式
+                // Exception e = new Exception("this is a log");
+                // e.printStackTrace();
+                // 第二种方法 这种方法可以很方便的写入文件
+                // Log.e("this is a log",Log.getStackTraceString(new Throwable()));
+                try {
+                    String name_utf8 = "小鲤鱼\uD83E\uDD80历\uD83E\uDD80险\uD83E\uDD80记\uD83E\uDD80";
+                    String name_gbk = new String(name_utf8.getBytes("GBK"), "GBK");
+                    String name_2312 = new String(name_utf8.getBytes("GBK"), "GB2312");
+
+                    if (name_utf8.length() >= 8) {
+                        binding.btnTestPrintStack.setText(String.format("%s...", subString(name_utf8, 0, 7)));
+                    } else {
+                        binding.btnTestPrintStack.setText(name_utf8);
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    // 获取
+    private String subString(String input, int start, int end) {
+        int unicodeCount = codePointCount(input, start, end);             // 获取unicode个数
+        int offsetIndex = input.offsetByCodePoints(start, unicodeCount); // 根据unicode个数获取偏移位置
+        return input.substring(start, offsetIndex);
+    }
+
+    public static int codePointCount(CharSequence seq, int beginIndex, int endIndex) {
+        int length = seq.length();
+        if (beginIndex < 0 || endIndex > length || beginIndex > endIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+        int n = endIndex - beginIndex;
+        for (int i = beginIndex; i < endIndex; ) {
+            if (isHighSurrogate(seq.charAt(i++)) && i < endIndex &&
+                    isLowSurrogate(seq.charAt(i))) {
+                n--;
+                i++;
+            }
+        }
+        return n;
+    }
+
+    public static int offsetByCodePoints(CharSequence seq, int index,
+                                         int codePointOffset) {
+        int length = seq.length();
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int x = index;
+        if (codePointOffset >= 0) {
+            int i;
+            for (i = 0; x < length && i < codePointOffset; i++) {
+                if (isHighSurrogate(seq.charAt(x++)) && x < length &&
+                        isLowSurrogate(seq.charAt(x))) {
+                    x++;
+                }
+            }
+            if (i < codePointOffset) {
+                throw new IndexOutOfBoundsException();
+            }
+        } else {
+            int i;
+            for (i = codePointOffset; x > 0 && i < 0; i++) {
+                if (isLowSurrogate(seq.charAt(--x)) && x > 0 &&
+                        isHighSurrogate(seq.charAt(x-1))) {
+                    x--;
+                }
+            }
+            if (i < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        return x;
     }
 
     @Override
