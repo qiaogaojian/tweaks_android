4cfbcd1acc093744c27eb69999e85ff95df818b7
qiaogaojian <qiaogaojian@vip.qq.com>
2021-01-11 17:03:51 +0800

    添加模型加载和GLSurfaceView渲染器

---
+++ app/src/main/java/com/etatech/test/opengl/Model.java
@@ -0,0 +1,84 @@
+package com.etatech.test.opengl;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+/**
+ * Created by Michael
+ * Date:  2021/1/11
+ * Desc:
+ */
+public class Model {
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+    private short drawOrder[];
+    private float vertexs[];
+    private float color[];
+    private int shaderProgram = -1;
+
+    private int mPositionHandle;
+    private int mColorHandle;
+    private int vertexCount;
+    private final int COORDS_PER_VERTEX = 3;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    public Model(float vertexs[], short drawOrder[], float color[], String vscode, String fscode) {
+        this.drawOrder = drawOrder;
+        this.vertexs = vertexs;
+        this.color = color;
+
+        vertexCount = vertexs.length / COORDS_PER_VERTEX;
+
+        // 初始化顶点缓冲
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertexs.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vertexs);
+        vertexBuffer.position(0);
+        // 初始化索引缓冲
+        ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
+
+        loadShader(vscode,fscode);
+    }
+
+    public void loadShader(String vscode, String fscode) {
+        if (shaderProgram != -1) {
+            return ;
+        }
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vscode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fscode);
+        shaderProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(shaderProgram, vertexShader);
+        GLES20.glAttachShader(shaderProgram, fragmentShader);
+        GLES20.glLinkProgram(shaderProgram);
+    }
+
+    private int loadShader(int type, String code) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+        return shader;
+    }
+
+    public void draw() {
+        GLES20.glUseProgram(shaderProgram);
+
+        mPositionHandle = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+        mColorHandle = GLES20.glGetUniformLocation(shaderProgram, "vColor");
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+}
--- app/src/main/java/com/etatech/test/opengl/MyGLRender.java
+++ app/src/main/java/com/etatech/test/opengl/MyGLRender.java
@@ -14,20 +14,31 @@ import javax.microedition.khronos.opengles.GL10;
  * Date:  2021/1/11
  * Desc:
  */
-public class MyGLRender implements GLSurfaceView.Renderer {
+public abstract class MyGLRender implements GLSurfaceView.Renderer {
+    protected String bgColor = "#0080807B";
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        float[] color = Tools.hex2Color("#0013C7C2");
+        onStart();
+        float[] color = Tools.hex2Color(bgColor);
         GLES20.glClearColor(color[0], color[1], color[2], color[3]);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        onChange();
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        onUpdate();
     }
+
+    public abstract void onStart();
+
+    public abstract void onChange();
+
+    public abstract void onUpdate();
 }
