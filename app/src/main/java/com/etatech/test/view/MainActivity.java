302b5ad451f3794dc5a3580d2cc563d4bad86fc0
qiaogaojian <qiaogaojian@vip.qq.com>
2021-01-11 11:57:58 +0800

    添加 opengl es 测试页面

--- app/src/main/java/com/etatech/test/view/MainActivity.java
+++ app/src/main/java/com/etatech/test/view/MainActivity.java
@@ -287,6 +287,13 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> implements V
                 startActivity(intent);
             }
         });
+        ClickUtil.setOnClick(binding.btnTestOpengl, new Action1() {
+            @Override
+            public void call(Object o) {
+                intent.setClass(MainActivity.this, TestOpenglesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
