c49ef2d12328d60eac7904988aec1941e99cc380
qiaogaojian <qiaogaojian@vip.qq.com>
2021-01-11 18:13:34 +0800

    添加shader变量设置工具函数

--- app/src/main/java/com/etatech/test/opengl/Model.java
+++ app/src/main/java/com/etatech/test/opengl/Model.java
@@ -26,6 +26,10 @@ public class Model {
     private final int COORDS_PER_VERTEX = 3;
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
 
+    public int getShaderProgram() {
+        return shaderProgram;
+    }
+
     public Model(float vertexs[], short drawOrder[], float color[], String vscode, String fscode) {
         this.drawOrder = drawOrder;
         this.vertexs = vertexs;
@@ -46,12 +50,12 @@ public class Model {
         drawListBuffer.put(drawOrder);
         drawListBuffer.position(0);
 
-        loadShader(vscode,fscode);
+        loadShader(vscode, fscode);
     }
 
     public void loadShader(String vscode, String fscode) {
         if (shaderProgram != -1) {
-            return ;
+            return;
         }
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vscode);
         int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fscode);
@@ -75,10 +79,69 @@ public class Model {
         GLES20.glEnableVertexAttribArray(mPositionHandle);
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
 
-        mColorHandle = GLES20.glGetUniformLocation(shaderProgram, "vColor");
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
-        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length,GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+        setVec4("vColor",color);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
 
         GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+
+    public void setBool(String name, boolean value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform1i(location, value ? 1 : 0);
+    }
+
+    public void setInt(String name, int value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform1i(location, value);
+    }
+
+    public void setFloat(String name, float value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform1f(location, value);
+    }
+
+    public void setVec2(String name, float[] value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform2fv(location, 1, value, 0);
+    }
+
+    public void setVec2(String name, float x, float y) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform2f(location, x, y);
+    }
+
+    public void setVec3(String name, float[] value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform3fv(location, 1, value, 0);
+    }
+
+    public void setVec3(String name, float x, float y, float z) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform3f(location, x, y, z);
+    }
+
+    public void setVec4(String name, float[] value) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform4fv(location, 1, value, 0);
+    }
+
+    public void setVec4(String name, float x, float y, float z, float w) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniform4f(location, x, y, z, w);
+    }
+
+    public void setMat2(String name, float[] mat) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniformMatrix2fv(location, 1,false, mat, 0);
+    }
+
+    public void setMat3(String name, float[] mat) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniformMatrix3fv(location, 1,false, mat, 0);
+    }
+
+    public void setMat4(String name, float[] mat) {
+        int location = GLES20.glGetUniformLocation(shaderProgram, name);
+        GLES20.glUniformMatrix4fv(location, 1,false, mat, 0);
     }
 }
