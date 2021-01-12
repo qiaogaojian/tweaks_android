e2779dd72eb293cef37cf7dff086f0e522e3fbd7
qiaogaojian <qiaogaojian@vip.qq.com>
2021-01-12 11:19:32 +0800

    构造正方形顶点数据

--- app/src/main/java/com/etatech/test/opengl/GLRender3Rect.java
+++ app/src/main/java/com/etatech/test/opengl/GLRender3Rect.java
@@ -1,5 +1,6 @@
 package com.etatech.test.opengl;
 
+import android.opengl.GLSurfaceView;
 import android.os.SystemClock;
 
 import com.etatech.test.utils.App;
@@ -13,11 +14,12 @@ import com.etatech.test.utils.Tools;
 
 public class GLRender3Rect extends MyGLRender {
     private Model model;
+    // 正方形
     static float vertexs[] = {
-            -0.5f, 0.5f, 0.0f,      // top left
-            -0.5f, -0.5f, 0.0f,     // bottom left
-            0.5f, -0.5f, 0.0f,      // bottom right
-            0.5f, 0.5f, 0.0f};      // top right
+            -0.8f,  0.8f/App.getInstance().designRatio, 0.0f,      // top left
+            -0.8f, -0.8f/App.getInstance().designRatio, 0.0f,     // bottom left
+             0.8f, -0.8f/App.getInstance().designRatio, 0.0f,      // bottom right
+             0.8f,  0.8f/App.getInstance().designRatio, 0.0f};      // top right
 
     private short drawOrder[] = { 0, 1, 2, 0, 2, 3 };
     private float color[] = {0.63671875f, 0.76953125f, 0.22265625f, 1.0f};
