bc2adb479b07480aa58d32b2e757af7068920a3c
qiaogaojian <qiaogaojian@gmail.com>
2021-03-06 23:51:24 +0800

    fix: 解决引用错误

--- app/src/main/java/com/etatech/test/widget/wallpaper/LiveWallpaperScreen.java
+++ app/src/main/java/com/etatech/test/widget/wallpaper/LiveWallpaperScreen.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
 import javax.microedition.khronos.opengles.GL10;
 
