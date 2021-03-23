ea8acb2b65f8b02a323ba4cea90b5d8f6cc587f4
qiaogaojian <qiaogaojian@vip.qq.com>
2021-03-23 09:51:18 +0800

    feat:  Java 原生测试迁移到 AndroidX

---
+++ app/src/main/java/com/etatech/test/java/blockchain/Blockchain.java
@@ -0,0 +1,19 @@
+package com.etatech.test.java.blockchain;
+
+/**
+ * Created by QiaoGaojian
+ * Date:  2021/3/23
+ * Desc:
+ */
+
+public class Blockchain {
+
+    public static void main(String args[]) {
+        hello();
+    }
+
+    public static void hello() {
+        int num = 666;
+        System.out.println(String.format("Hello World %d", num));
+    }
+}
