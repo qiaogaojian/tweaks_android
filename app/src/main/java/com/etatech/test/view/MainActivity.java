85285ecbd9a065a6157c543e79c83497437f57a8
qiaogaojian <qiaogaojian@vip.qq.com>
2022-11-04 18:48:21 +0800

    config: upgrade sdk

--- app/src/main/java/com/etatech/test/view/MainActivity.java
+++ app/src/main/java/com/etatech/test/view/MainActivity.java
@@ -23,7 +23,6 @@ import android.content.res.Resources;
 
 import androidx.databinding.DataBindingUtil;
 
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
