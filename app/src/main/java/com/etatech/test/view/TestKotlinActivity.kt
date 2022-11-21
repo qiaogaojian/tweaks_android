46c28bd16ea81f9dc320bd34c385150a247dccbb
qiaogaojian <qiaogaojian@vip.qq.com>
2022-11-21 16:23:08 +0800

    test: kotlin activity

--- app/src/main/java/com/etatech/test/view/TestKotlinActivity.kt
+++ app/src/main/java/com/etatech/test/view/TestKotlinActivity.kt
@@ -2,9 +2,18 @@ package com.etatech.test.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.etatech.test.R
+import kotlinx.android.synthetic.main.activity_test_kotlin.*
 
 class TestKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_kotlin)
+
+        btn1.setOnClickListener {
+            Toast.makeText(this, "You click Button 1", Toast.LENGTH_SHORT).show()
+        }
     }
 }
